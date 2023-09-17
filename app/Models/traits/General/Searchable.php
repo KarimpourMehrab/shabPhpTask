e5429134ab616522f8caacfbe6c012887cd71217
mehrab.karimpour <mehrab.karimpour@elanza.com>
2023-09-17 09:50:31 +0330

    (fix) remove extra imported codes

--- app/Models/traits/General/Searchable.php
+++ app/Models/traits/General/Searchable.php
@@ -3,8 +3,8 @@
 namespace App\Models\traits\General;
 
 
-use App\Services\Search\Elastic\Fast;
-use App\Services\Search\DeliveryService;
+use App\Services\Search\Elastic\Elastic;
+use App\Services\Search\SearchService;
 
 trait Searchable
 {
@@ -14,23 +14,14 @@ trait Searchable
     private static array $searchAbleFields = ['title'];
     private static string $indexName = 'products';
 
-//    public function index(bool $shouldBeSync = false): void
-//    {
-//        if ($shouldBeSync) {
-//            ProductElasticJob::dispatchSync(self::$indexName, $this->id);
-//        } else {
-//            ProductElasticJob::dispatch(self::$indexName, $this->id);
-//        }
-//    }
-
     /**
      * @param string|null $searchService
      * @return mixed
      */
     public static function searchService(string|null $searchService = null): mixed
     {
-        $searchService = $searchService ?? Fast::name();
-        $searchService = DeliveryService::make($searchService);
+        $searchService = $searchService ?? Elastic::name();
+        $searchService = SearchService::make($searchService);
         $searchService->setIndex(self::$indexName);
         $searchService->setFields(self::$searchAbleFields);
         return $searchService;
